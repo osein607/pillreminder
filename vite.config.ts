86481f0dd4268a9f4f68b90ed5f1c8e2f1cf640a
osein607 <osein607@naver.com>
2025-11-20 12:53:38 +0900

    ⚡️빌드 결과물 압축하여 배포 용량 줄이도록 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import compression from "vite-plugin-compression";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    compression(), // ⭐ gzip 생성
+    // 필요하면 brotli 추가 → compression({ algorithm: 'brotliCompress' })
+  ],
+});
